86c100ec68db3ea367073404887f20647868b37e
Oleksandr <burtovyi@gmail.com>
2025-03-26 23:52:38 +0100

    HW - Done

--- app/schemas.py
+++ app/schemas.py
@@ -1,13 +1,15 @@
+# app/schemas.py
 from pydantic import BaseModel, EmailStr
-from datetime import date
+from typing import Optional
+from datetime import datetime
 
+# Схеми для контактів
 class ContactBase(BaseModel):
     first_name: str
     last_name: str
     email: EmailStr
-    phone: str
-    birthday: date
-    additional_data: str = None
+    phone: Optional[str] = None
+    birthday: Optional[datetime] = None
 
 class ContactCreate(ContactBase):
     pass
@@ -17,6 +19,33 @@ class ContactUpdate(ContactBase):
 
 class ContactOut(ContactBase):
     id: int
-
     class Config:
         orm_mode = True
+
+# Схеми для користувачів
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserOut(UserBase):
+    id: int
+    is_active: bool
+    is_verified: bool
+    avatar_url: Optional[str] = None
+    class Config:
+        orm_mode = True
+
+# Схеми для аутентифікації
+class Login(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
